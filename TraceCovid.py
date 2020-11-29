486dda78cf43fbc30fdc7c3d4f77563ca0f80781
deepanshugautamgit <68638539+deepanshugautamgit@users.noreply.github.com>
2020-11-29 17:38:20 +0530

    rearranging the code and adding comments

--- TraceCovid.py
+++ TraceCovid.py
@@ -3,103 +3,6 @@ import pandas as pd
 import os
 
 
-#connection to sqlite
-conn=sqlite3.connect('TraceCovid.db')
-
-
-#cursor
-c=conn.cursor()
-
-
-#tables script
-c.executescript('''Create table if not exists delhi (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (95),CONTACT  int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
-
-create table if not exists mumbai (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (115),CONTACT int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
-
-create table if not exists kolkata (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (115),CONTACT int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
-
-create table if not exists chennai (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (115),CONTACT int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
-
-create table if not exists CASES(SNO int,NAME_OF_CITY varchar(17),TOTAL_CASES INT(37),RECOVERED_CASES INT(40),DEATHS INT(35));
-
-CREATE TABLE IF NOT EXISTS PRICES(SNO INT,ISOLATION_BEDS VARCHAR(45),ICU_BEDS_WITH_VENTILATOR VERCHAR(45),ICU_BEDS_WITHOUT_VENTILATOR VARCHAR(45));
-
-create table if not exists INDIA(TOTAL_CASES varchar,RECOVERED varchar,DEATHS varchar);
-
-create table if not exists user_data(SNO int, PASSWD varchar(30) default'000');
-''')
-conn.commit()
-
-
-#Inserting values in tables
-#run only once
-"""
-c.executescript('insert into INDIA values("8.04M","7.32M","121K")')
-conn.commit()
-
-
-
-c.execute('insert into user_data values(1,"root");')
-conn.commit()
-
-c.executescript('''insert into delhi values(1,'LOK NAYAK HOSPITAL','JAWAHARLAL NEHRU MARG, NEW DELHI-110002',1123236000,2000,1276,73.37,295);
-
-insert into delhi values(2,'RAJIV GANDHI SUPER SPECIALITY HOSPITA;','TAHIRPUR VILLAGE, DILSHAD GARDEN, NEW DELHI-110093',01122890604,1990,750,69.32,310);
-
-insert into delhi values(3,'RAM MANOHAR LOHIA HOSPITAL','BABA KHARAK SINGH ROAD,CONNAUGHT PLACE, NEW DELHI-110001',01123365525,236,92,44.28,122);
-
-insert into delhi values(4,'St. STEPHENS HOSPITAL','TIS HAZARI, NEW DELHI-110054',0112396021,250,192,68.89,198);
-''')
-conn.commit()
-
-
-
-
-c.executescript('''insert into mumbai values(1,'BOMBAY HOSPITAL AND MEDICAL RESEARCH CENTRE','12 VITTHALDAS THACKERSEY MARG, NEW MARINE LINES, MUMBAI, MAHARASHTRA-400020',0222206767,90,11,78.43,34);
-
-insert into mumbai values(2,'SAIFEE HOSPITAL','15/17 MAHARASHI KARVE ROAD,CHARNI ROAD EAST,GURGAON,MUMBAI,MAHARASHTRA-400004',0226757011,97,13,69.99,42);
-
-insert into mumbai values(3,'BHATIA HOSPITAL','TARDEO ROAD,OLD CHIKALWADI,GRANT ROAD WEST,MUMBAI,MAHARASHTRA-400007',0226666000,80,10,88.4,23);
-
-insert into mumbai values(4,'WOCKHARDT HOSPITAL','POLICE STATION,1877DOCTOR ANANDRAO NAIR MARG,MUMBAI CENTRAL,MUMBAI,MAHARASHTRA-400011',0829110100,128,30,88.8,19);
-''')
-conn.commit()
-
-c.executescript('''insert into kolkata values(1,'M R BANGUR HOSPITAL','241-DESHPARAN SASMAL ROAD,NETAJI NAGAR RAJENDRA PRASAD COLONY,TOLLYGUNGE,KOLKATA,WEST BENGAL-700033',22202070,670,185,76.94,333);
-
-insert into kolkata values(2,'DESUN HOSPITAL','DESUN MORE,720,EASTERN METROPOLITAN BYPASS,GOL PARK,SECTOR-I KASBA,KOLKATA,WEST BENGAL-700107',90517151,113,60,66.78,267);
-
-insert into kolkata values(3,'AMRI HOSPITAL','16 17,JC BLOCK LANE,BROADWAY ROAD,OPPOSITE SALT LAKE,SECTOR III,BIDHANNAGAR,KOLKATA,WEST BENGAL-700098',3366063,72,9,70.11,301);
-
-insert into kolkata values(4,'BELEGHATA ID AND BG HOSPITAL','57,BELEGHATA MAIN RD, SUBHAS SAROBAR PARK,PHOOL BAGAN,BELEGHATA,KOLKATA,WEST BENGAL-700010',33303200,115,9,77.23,356);
-''')
-conn.commit()
-
-c.executescript('''insert into chennai values(1,'Rajiv Gandhi Government General Hospital','GH Post Office, 3, Poonamallee High Road, Near Chennai Central, near Park Town, Chennai, Tamil Nadu 600003',04425305000,1600,800,81,315);
-
-insert into chennai values(2,' HOSPITAL','822, Poonamallee High Rd, near Ega Theatre, Kilpauk, Chennai, Tamil Nadu 600010',04426412979,500,100,70.35,153);
-
-''')
-conn.commit()
-
-c.executescript('''INSERT INTO CASES VALUES(1,'DELHI',359000,327000,6312);
-INSERT INTO CASES VALUES(2,'MUMBAI',1654028,1478496,43463);
-INSERT INTO CASES VALUES(3,'KOLKATA',157000,144000,3074);
-INSERT INTO CASES VALUES(4,'CHENNAI',397800,314700,6604);
-
-''')
-conn.commit()
-
-c.executescript('''INSERT INTO PRICES VALUES(1,'Rs8000-Rs10000','Rs15000-Rs18000','Rs13000-Rs15000');
-INSERT INTO PRICES VALUES(2,'Rs4000-Rs6000','Rs9000-Rs11000','Rs7500-Rs9500');
-INSERT INTO PRICES VALUES(3,'Rs3000-Rs5000','Rs10000-Rs15000','Rs9000-Rs13500');
-INSERT INTO PRICES VALUES(4,'Rs3000-Rs5000','Rs12000-Rs15000','Rs8000-Rs12000' );
-''')
-conn.commit()
-"""
-
-
-
 #main menu
 def home():
     print('''#======================================================================================================#
@@ -811,12 +714,114 @@ def changePass():
         print('WRONG PASSWORD')
         changePass()
 
+
 #__main__
+
+
+#connection to sqlite
+conn=sqlite3.connect('TraceCovid.db')
+
+
+#cursor
+c=conn.cursor()
+
+
+#tables script
+c.executescript('''Create table if not exists delhi (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (95),CONTACT  int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
+
+create table if not exists mumbai (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (115),CONTACT int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
+
+create table if not exists kolkata (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (115),CONTACT int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
+
+create table if not exists chennai (SNO integer , NAME_OF_HOSPITAL varchar(85),ADDRESS_OF_HOSPITAL varchar (115),CONTACT int(35),TOTAL_BEDS int (37),VACANT_BEDS int (37),RECOVERY_RATE float ,TOTAL_DEATHS int(27));
+
+create table if not exists CASES(SNO int,NAME_OF_CITY varchar(17),TOTAL_CASES INT(37),RECOVERED_CASES INT(40),DEATHS INT(35));
+
+CREATE TABLE IF NOT EXISTS PRICES(SNO INT,ISOLATION_BEDS VARCHAR(45),ICU_BEDS_WITH_VENTILATOR VERCHAR(45),ICU_BEDS_WITHOUT_VENTILATOR VARCHAR(45));
+
+create table if not exists INDIA(TOTAL_CASES varchar,RECOVERED varchar,DEATHS varchar);
+
+create table if not exists user_data(SNO int, PASSWD varchar(30) default'000');
+''')
+conn.commit()
+
+
+#Inserting values in tables
+#run only once
+"""
+c.executescript('insert into INDIA values("8.04M","7.32M","121K")')
+conn.commit()
+
+
+
+c.execute('insert into user_data values(1,"root");')
+conn.commit()
+
+c.executescript('''insert into delhi values(1,'LOK NAYAK HOSPITAL','JAWAHARLAL NEHRU MARG, NEW DELHI-110002',1123236000,2000,1276,73.37,295);
+
+insert into delhi values(2,'RAJIV GANDHI SUPER SPECIALITY HOSPITA;','TAHIRPUR VILLAGE, DILSHAD GARDEN, NEW DELHI-110093',01122890604,1990,750,69.32,310);
+
+insert into delhi values(3,'RAM MANOHAR LOHIA HOSPITAL','BABA KHARAK SINGH ROAD,CONNAUGHT PLACE, NEW DELHI-110001',01123365525,236,92,44.28,122);
+
+insert into delhi values(4,'St. STEPHENS HOSPITAL','TIS HAZARI, NEW DELHI-110054',0112396021,250,192,68.89,198);
+''')
+conn.commit()
+
+
+
+
+c.executescript('''insert into mumbai values(1,'BOMBAY HOSPITAL AND MEDICAL RESEARCH CENTRE','12 VITTHALDAS THACKERSEY MARG, NEW MARINE LINES, MUMBAI, MAHARASHTRA-400020',0222206767,90,11,78.43,34);
+
+insert into mumbai values(2,'SAIFEE HOSPITAL','15/17 MAHARASHI KARVE ROAD,CHARNI ROAD EAST,GURGAON,MUMBAI,MAHARASHTRA-400004',0226757011,97,13,69.99,42);
+
+insert into mumbai values(3,'BHATIA HOSPITAL','TARDEO ROAD,OLD CHIKALWADI,GRANT ROAD WEST,MUMBAI,MAHARASHTRA-400007',0226666000,80,10,88.4,23);
+
+insert into mumbai values(4,'WOCKHARDT HOSPITAL','POLICE STATION,1877DOCTOR ANANDRAO NAIR MARG,MUMBAI CENTRAL,MUMBAI,MAHARASHTRA-400011',0829110100,128,30,88.8,19);
+''')
+conn.commit()
+
+c.executescript('''insert into kolkata values(1,'M R BANGUR HOSPITAL','241-DESHPARAN SASMAL ROAD,NETAJI NAGAR RAJENDRA PRASAD COLONY,TOLLYGUNGE,KOLKATA,WEST BENGAL-700033',22202070,670,185,76.94,333);
+
+insert into kolkata values(2,'DESUN HOSPITAL','DESUN MORE,720,EASTERN METROPOLITAN BYPASS,GOL PARK,SECTOR-I KASBA,KOLKATA,WEST BENGAL-700107',90517151,113,60,66.78,267);
+
+insert into kolkata values(3,'AMRI HOSPITAL','16 17,JC BLOCK LANE,BROADWAY ROAD,OPPOSITE SALT LAKE,SECTOR III,BIDHANNAGAR,KOLKATA,WEST BENGAL-700098',3366063,72,9,70.11,301);
+
+insert into kolkata values(4,'BELEGHATA ID AND BG HOSPITAL','57,BELEGHATA MAIN RD, SUBHAS SAROBAR PARK,PHOOL BAGAN,BELEGHATA,KOLKATA,WEST BENGAL-700010',33303200,115,9,77.23,356);
+''')
+conn.commit()
+
+c.executescript('''insert into chennai values(1,'Rajiv Gandhi Government General Hospital','GH Post Office, 3, Poonamallee High Road, Near Chennai Central, near Park Town, Chennai, Tamil Nadu 600003',04425305000,1600,800,81,315);
+
+insert into chennai values(2,' HOSPITAL','822, Poonamallee High Rd, near Ega Theatre, Kilpauk, Chennai, Tamil Nadu 600010',04426412979,500,100,70.35,153);
+
+''')
+conn.commit()
+
+c.executescript('''INSERT INTO CASES VALUES(1,'DELHI',359000,327000,6312);
+INSERT INTO CASES VALUES(2,'MUMBAI',1654028,1478496,43463);
+INSERT INTO CASES VALUES(3,'KOLKATA',157000,144000,3074);
+INSERT INTO CASES VALUES(4,'CHENNAI',397800,314700,6604);
+
+''')
+conn.commit()
+
+c.executescript('''INSERT INTO PRICES VALUES(1,'Rs8000-Rs10000','Rs15000-Rs18000','Rs13000-Rs15000');
+INSERT INTO PRICES VALUES(2,'Rs4000-Rs6000','Rs9000-Rs11000','Rs7500-Rs9500');
+INSERT INTO PRICES VALUES(3,'Rs3000-Rs5000','Rs10000-Rs15000','Rs9000-Rs13500');
+INSERT INTO PRICES VALUES(4,'Rs3000-Rs5000','Rs12000-Rs15000','Rs8000-Rs12000' );
+''')
+conn.commit()
+"""
+
+
 #password
 realp=c.execute('SELECT PASSWD from user_data;').fetchone()
 
+
 home()
 
+
 c.close()
+
 
 #MADE BY DEEPANSHU AND MANIYA OF XII-A
